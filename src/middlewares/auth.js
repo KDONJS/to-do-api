7c6ba85b20fd745e8b6e9ece2822e50dc96112a2
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-17 18:56:06 -0500

    primera version de creacion de usuarios y auth  - v.0.2.3 (develop)

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,21 +2,25 @@ const jwt = require('jsonwebtoken');
 const Usuario = require('../models/usuarioModel');
 
 const auth = async (req, res, next) => {
-    try {
-      const token = req.header('Authorization').replace('Bearer ', '');
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const usuario = await Usuario.findOne({ _id: decoded._id, 'tokens.token': token });
-  
-      if (!usuario) {
-        throw new Error();
-      }
-  
-      req.token = token;
-      req.usuario = usuario;
-      next();
-    } catch (error) {
-      res.status(401).send({ error: 'Por favor autentícate.' });
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Busca un usuario con el ID decodificado y que tenga el token en su arreglo de tokens.
+    const usuario = await Usuario.findOne({ 
+      _id: decoded._id, 
+      'tokens.token': token 
+    });
+
+    if (!usuario) {
+      throw new Error('No se pudo encontrar el usuario con el token proporcionado.');
     }
-  };
-  
-  module.exports = auth;
+
+    req.usuario = usuario;
+    req.token = token; // Guarda el token en el request para uso posterior.
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Por favor autentícate.' });
+  }
+};
+
+module.exports = auth;
