2f4741288037197a67d292b9899502d52992a110
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-17 17:19:26 -0500

    primera version de creacion de usuarios y auth  - v.0.1.7 (develop)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,28 +5,23 @@ const jwt = require('jsonwebtoken');
 
 exports.registrarUsuario = async (req, res) => {
   try {
-    // Crear un nuevo usuario con el modelo Usuario
     const usuario = new Usuario(req.body);
-
-    // Hashear la contraseña antes de guardar el usuario
     usuario.password = await bcrypt.hash(usuario.password, 8);
-    
-    // Guardar el usuario en la base de datos
     await usuario.save();
-
-    // Crear un token para el nuevo usuario
     const token = jwt.sign({ _id: usuario._id.toString() }, process.env.JWT_SECRET);
-
-    // Enviar el perfil del usuario y el token como respuesta
     res.status(201).send({ usuario, token });
   } catch (error) {
-    res.status(400).send(error);
+    if (error.name === 'ValidationError') {
+      return res.status(400).send({ error: 'Datos de entrada inválidos.' });
+    }
+    console.error('Error al registrar el usuario:', error);
+    res.status(500).send({ error: 'Error interno del servidor al registrar usuario.' });
   }
 };
 
+
 exports.iniciarSesionUsuario = async (req, res) => {
   try {
-    // Buscar el usuario por email
     const usuario = await Usuario.findOne({ email: req.body.email });
     if (!usuario) {
       console.log('Usuario no encontrado con el email:', req.body.email);
@@ -35,24 +30,17 @@ exports.iniciarSesionUsuario = async (req, res) => {
 
     console.log('Password de la petición:', req.body.password);
     console.log('Hash de la contraseña en la base de datos:', usuario.password);
-    console.log('JWT_SECRET:', process.env.JWT_SECRET);
-    
-    // Verificar la contraseña
+
     const isMatch = await bcrypt.compare(req.body.password, usuario.password);
     if (!isMatch) {
       console.log('Contraseña incorrecta para el usuario:', req.body.email);
       return res.status(400).send({ error: 'Datos de inicio de sesión inválidos' });
     }
 
-    // Crear un token para el usuario existente
     const token = jwt.sign({ _id: usuario._id.toString() }, process.env.JWT_SECRET);
-
-    // Enviar el perfil del usuario y el token como respuesta
     res.send({ usuario, token });
   } catch (error) {
     console.error('Error durante el inicio de sesión:', error);
-    res.status(500).send(error);
+    res.status(500).send({ error: 'Error interno del servidor al iniciar sesión.' });
   }
-};
-
-  
+};  
