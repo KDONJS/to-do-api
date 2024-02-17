84b32c2f7c909a339284d47ab6e6aadfa3197a91
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-17 18:34:28 -0500

    primera version de creacion de usuarios y auth  - v.0.1.9 (develop)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,23 +24,16 @@ exports.iniciarSesionUsuario = async (req, res) => {
   try {
     const usuario = await Usuario.findOne({ email: req.body.email });
     if (!usuario) {
-      console.log('Usuario no encontrado con el email:', req.body.email);
-      return res.status(400).send({ error: 'Datos de inicio de sesión inválidos' });
+      return res.status(400).send({ error: 'Usuario no encontrado.' });
     }
 
-    console.log('Password de la petición:', req.body.password);
-    console.log('Hash de la contraseña en la base de datos:', usuario.password);
-
-    const isMatch = bcrypt.compareSync(req.body.password, usuario.password);
-    if (!isMatch) {
-      console.log('Contraseña incorrecta para el usuario:', req.body.email);
-      return res.status(400).send({ error: 'Datos de inicio de sesión inválidos' });
+    const isMatch = await bcrypt.compare(req.body.password, usuario.password);
+    if (isMatch) {
+      res.send({ message: 'Inicio de sesión exitoso.' });
+    } else {
+      res.status(400).send({ error: 'Contraseña incorrecta.' });
     }
-
-    const token = jwt.sign({ _id: usuario._id.toString() }, process.env.JWT_SECRET);
-    res.send({ usuario, token });
   } catch (error) {
-    console.error('Error durante el inicio de sesión:', error);
-    res.status(500).send({ error: 'Error interno del servidor al iniciar sesión.' });
+    res.status(500).send({ error: 'Error al iniciar sesión.' });
   }
-};  
+};
