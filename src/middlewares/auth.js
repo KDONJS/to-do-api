d42b24cb7903fe91d30a1674f2bd0e7377c4dfcd
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-17 18:37:48 -0500

    primera version de creacion de usuarios y auth  - v.0.1.9 (develop)

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,18 +5,18 @@ const auth = async (req, res, next) => {
     try {
       const token = req.header('Authorization').replace('Bearer ', '');
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const usuario = await Usuario.findOne({ _id: decoded._id, 'tokens.token': token });
-  
+      const usuario = await Usuario.findOne({ _id: decoded._id });
+
       if (!usuario) {
-        throw new Error();
+        throw new Error('Usuario no encontrado.');
       }
-  
-      req.token = token;
+
       req.usuario = usuario;
+      req.token = token; // Opcional, solo si necesitas el token más adelante.
       next();
     } catch (error) {
       res.status(401).send({ error: 'Por favor autentícate.' });
     }
-  };
-  
-  module.exports = auth;
+};
+
+module.exports = auth;
