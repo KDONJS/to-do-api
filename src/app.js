6569136e16da304984360398ccab6fe0cd5f202c
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-22 00:56:00 -0500

    mejoras en el loguin de la api - v1

--- src/app.js
+++ src/app.js
@@ -37,8 +37,4 @@ app.get('/', (req, res) => {
 });
 
   
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
+module.exports = app;
