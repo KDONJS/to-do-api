96c1c6adcfac37ad160bcc27ab06a1763fde338f
Yorlin Quispe Ygnacio <yquispey@bcds.net>
2024-02-22 23:51:06 -0500

    mejoras en el manejo de front y carag de imagenes

--- src/app.js
+++ src/app.js
@@ -32,10 +32,4 @@ app.get('/', (req, res) => {
 });
 
   
-//module.exports = app;
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
+module.exports = app;
